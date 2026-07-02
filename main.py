c2da35d499bf38df1fb76b612ba84054195434a9
olsherif <92591038+olsherif@users.noreply.github.com>
2026-07-02 23:23:45 +0300

    Update uvicorn.run to use 'main:app' syntax

--- main.py
+++ main.py
@@ -81,4 +81,5 @@ async def root():
     return {"status": "Server is running perfectly"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT)
+
